4abea8503ef9818a520204aed461cf31339f77ed
Hans W. Uhlig <huhlig@gmail.com>
2022-09-27 22:25:26 -0700

    Fixing Lifetime and Borrow Issues

--- examples/transparency.rs
+++ examples/transparency.rs
@@ -1,3 +1,4 @@
+use std::borrow::BorrowMut;
 use std::path::Path;
 use std::time::Duration;
 
@@ -10,11 +11,12 @@ use engine::{
     run,
     types::Vec2,
 };
+use engine::resource::ImageResourceSlice;
 
 const SCREEN_WIDTH: u32 = 1024;
 const SCREEN_HEIGHT: u32 = 768;
-const PIXELS_WIDTH: u32 = 1024/2;
-const PIXELS_HEIGHT: u32 = 768/2;
+const PIXELS_WIDTH: u32 = 1024 / 2;
+const PIXELS_HEIGHT: u32 = 768 / 2;
 
 pub struct Demo {
     ctx: Context,
@@ -38,29 +40,29 @@ impl GameState for Demo {
         engine.resize_buffer(PIXELS_WIDTH, PIXELS_HEIGHT);
         self.image_handle_1 = Some(
             engine
-            .resource_manager
-            .load_image(Path::new("resources/images/test_pattern_1.bmp"))
+                .resource_manager
+                .load_image(Path::new("resources/images/test_pattern_1.bmp"))
         );
         self.image_handle_2 = Some(
             engine
-            .resource_manager
-            .load_image(Path::new("resources/images/test_logo.png"))
+                .resource_manager
+                .load_image(Path::new("resources/images/test_logo.png"))
         );
         true
     }
     fn on_update(&mut self, elapsed_time: Duration, engine: &mut Engine) -> bool {
         engine.window.set_title(&format!("{}ms", elapsed_time.as_millis()));
         let image_1 = engine.resource_manager.get_image(self.image_handle_1.unwrap()).unwrap();
-        let mut screen = engine.screen();
+        let mut screen = engine.screen.borrow_mut();
         {
             for y in (0..PIXELS_HEIGHT).step_by(image_1.height() as usize) {
                 for x in (0..PIXELS_WIDTH).step_by(image_1.width() as usize) {
-                    blit(image_1, &mut screen, Vec2 { x: x as i32, y: y as i32 });
+                    blit(image_1, screen, Vec2 { x: x as i32, y: y as i32 });
                 }
             }
         }
         let image_2 = engine.resource_manager.get_image(self.image_handle_2.unwrap()).unwrap();
-        blit_with_alpha(image_2, &mut screen, Vec2 { x: 0, y: 0 });
+        blit_with_alpha(image_2, screen, Vec2 { x: 0, y: 0 });
         true
     }
     fn context(&self) -> &Context {
@@ -69,6 +71,6 @@ impl GameState for Demo {
 }
 
 fn main() {
-   let game_state = Demo::new();
-   run(game_state);
+    let game_state = Demo::new();
+    run(game_state);
 }
